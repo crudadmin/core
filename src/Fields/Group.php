02950569b5dd96ed4e91f87d6f68ce991070a93d
Marek Gogol <9mike66@gmail.com>
2019-07-04 15:54:12 +0200

    fixed columns type rendering

--- src/Fields/Group.php
+++ src/Fields/Group.php
@@ -25,11 +25,27 @@ class Group
     public $add = [];
 
     /*
+     * Group type
+     */
+    public $type = 'default';
+
+    /*
      * Boot group and add fields into class
      */
     public function __construct(array $fields = [])
     {
         $this->fields = $fields;
+    }
+
+    /*
+     * Set type of group
+     * group/tab
+     */
+    public function type($type = 'group')
+    {
+        $this->type = $type;
+
+        return $this;
     }
 }
 ?>
